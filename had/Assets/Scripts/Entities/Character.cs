52c2d638ae2b3ed54f93053ba03811819151ccd2
Lukas Bucher <lukas.bucherr@gmail.com>
2024-12-18 14:17:58 +0100

    1 | scenes and text

---
+++ had/Assets/Scripts/Entities/Character.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCharacter", menuName = "Data/New Character")]
+[System.Serializable]
+public class Character : ScriptableObject
+{
+    public string characterName;
+}
